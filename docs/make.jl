a4c5ec38691fcb4fc8473bb31ed7d3c0339c2e3d
Micah Rufsvold <mrufsvold@mathematica-mpr.com>
2022-11-29 14:56:16 -0500

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,10 @@ using Documenter, Normalize
 makedocs(
     sitename="Normalize.jl",
     modules = Module[Normalize],
-    pages = ["Index" => "index.md"]    
-    
-    )
+    pages = ["Contents" => "index.md"]    
+)
+
+deploydocs(
+    repo = "github.com/mrufsvold/Normalize.jl.git",
+    devbranch = "main"
+)
