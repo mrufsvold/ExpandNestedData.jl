e4834a3244bb0cdfebff564e90a5ad660091b85e
Micah Rufsvold <mrufsvold@mathematica-mpr.com>
2023-03-27 15:15:32 -0400

    add column name join option

--- src/ExpandNestedData.jl
+++ src/ExpandNestedData.jl
@@ -7,7 +7,7 @@ export ColumnDefinition
 export nested_columns, flat_columns
 
 # Link a list of keys into an underscore separted column name
-join_names(names) = names .|> string |> (s -> join(s, "_")) |> Symbol
+join_names(names, joiner="_") = names .|> string |> (s -> join(s, joiner)) |> Symbol
 
 include("ExpandTypes.jl")
 include("ExpandedTable.jl")
--- src/ExpandTypes.jl
+++ src/ExpandTypes.jl
@@ -183,8 +183,8 @@ ColumnDefs = Vector{ColumnDefinition}
 ## Returns
 `::ColumnDefinition`
 """
-function ColumnDefinition(field_path; column_name=nothing, flatten_arrays=false, default_value=missing, pool_arrays=false)
-    column_name = column_name isa Nothing ? join_names(field_path) : column_name
+function ColumnDefinition(field_path; column_name=nothing, flatten_arrays=false, default_value=missing, pool_arrays=false, name_join_pattern = "_")
+    column_name = column_name isa Nothing ? join_names(field_path, name_join_pattern) : column_name
     ColumnDefinition(field_path, 1, column_name, flatten_arrays, default_value, pool_arrays)
 end
 function ColumnDefinition(field_path, column_names::Dict; pool_arrays::Bool)
