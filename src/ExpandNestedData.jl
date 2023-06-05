0bbd811e11cfc2197184fc3819b52136a9f50a68
Micah Rufsvold <mrufsvold@mathematica-mpr.com>
2023-06-05 19:47:23 -0400

    Fix: include reference in main

--- src/ExpandNestedData.jl
+++ src/ExpandNestedData.jl
@@ -9,7 +9,7 @@ export nested_columns, flat_columns
 include("Utils.jl")
 include("NestedIterators.jl")
 include("ColumnSet.jl")
-include("_ExpandTypes.jl")
+include("ExpandTypes.jl")
 include("PathGraph.jl")
 include("ExpandedTable.jl")
 include("StackProcessing.jl")
