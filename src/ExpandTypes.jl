bf20627971e0df4150565a6afcd6dde447e97df5
Micah Rufsvold <mrufsvold@mathematica-mpr.com>
2023-06-06 09:39:57 -0400

    update docs to match columndefinition change

--- src/ExpandTypes.jl
+++ src/ExpandTypes.jl
@@ -46,7 +46,7 @@ pool_arrays(c::ColumnDefinition) = c.pool_arrays
     ColumnDefinition(field_path; column_name=nothing, flatten_arrays=false, default_value=missing, pool_arrays=false)
 
 ## Args
-* `field_path`: Vector of keys/fieldnames that constitute a path from the top of the data to the values to extract for the column
+* `field_path`: Vector or Tuple of keys/fieldnames that constitute a path from the top of the data to the values to extract for the column
 
 ## Keyword Args
 * `column_name::Symbol`: A name for the resulting column. If `nothing`, defaults to joining the field_path with snake_case_format.
