c4fdc7d680e8d1f748c57bbc3507600bbd2fe56d
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2023-07-07 14:04:34 -0400

    add some basic benchmarks to test against different branches

--- benchmarks/benchmarks.jl
+++ benchmarks/benchmarks.jl
@@ -7,18 +7,11 @@ small_dict = Dict(
     :c => Dict(:e => Symbol(3), :f => 4)
 )
 
-@btime expand(small_dict; lazy_columns=true, column_style=:nested)
-@profview_allocs expand(small_dict; lazy_columns=true, column_style=:nested)
-@profview ExpandNestedData.expand(small_dict; lazy_columns=true, column_style=:nested);
 
 many_records = [
     small_dict
-    for _ in 1:10
+    for _ in 1:1000
 ]
-@profview expand(many_records; lazy_columns=true, column_style=:nested)
-
-@btime expand($many_records; lazy_columns=true, column_style=:nested);
- 
 
 function make_deep_dict(depth=1)
     if depth == 1
@@ -29,6 +22,12 @@ function make_deep_dict(depth=1)
         for i in 1:3
     )
 end
-
-deep_dict = make_deep_dict(10)
-@btime expand($deep_dict; lazy_columns=true, column_style=:nested);
+                
+# @btime expand($small_dict; lazy_columns=true, column_style=:nested)
+# @profview ExpandNestedData.expand(small_dict; lazy_columns=true, column_style=:nested);
+# deep_dict = make_deep_dict(10)
+# @btime expand($deep_dict; lazy_columns=true, column_style=:nested);
+# @descend expand(many_records; lazy_columns=true, column_style=:nested)
+# @profview ExpandNestedData.expand(many_records; lazy_columns=true, column_style=:nested)
+# @profview_allocs ExpandNestedData.expand(many_records; lazy_columns=true, column_style=:nested)
+# @btime expand($many_records; lazy_columns=true, column_style=:nested);
