320003e5da1f360fcf1e03453cfbd851f01cf5c5
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2023-07-07 14:06:11 -0400

    catch up benchmarks to perfromance branch

--- benchmarks/benchmarks.jl
+++ benchmarks/benchmarks.jl
@@ -1,5 +1,4 @@
 using ExpandNestedData
-using TypedTables
 
 small_dict = Dict(
     :a => 1,
@@ -7,15 +6,11 @@ small_dict = Dict(
     :c => Dict(:e => Symbol(3), :f => 4)
 )
 
-expand(small_dict; lazy_columns=true, column_style=:nested);
 
 many_records = [
     small_dict
-    for _ in 1:100
+    for _ in 1:1000
 ]
-
-@btime expand(many_records; lazy_columns=true, column_style=:nested);
- 
 
 function make_deep_dict(depth=1)
     if depth == 1
@@ -26,6 +21,12 @@ function make_deep_dict(depth=1)
         for i in 1:3
     )
 end
-
-deep_dict = make_deep_dict(10)
-@btime expand(deep_dict; lazy_columns=true, column_style=:nested);
+                
+# @btime expand($small_dict; lazy_columns=true, column_style=:nested)
+# @profview ExpandNestedData.expand(small_dict; lazy_columns=true, column_style=:nested);
+# deep_dict = make_deep_dict(10)
+# @btime expand($deep_dict; lazy_columns=true, column_style=:nested);
+# @descend expand(many_records; lazy_columns=true, column_style=:nested)
+# @profview ExpandNestedData.expand(many_records; lazy_columns=true, column_style=:nested)
+# @profview_allocs ExpandNestedData.expand(many_records; lazy_columns=true, column_style=:nested)
+# @btime expand($many_records; lazy_columns=true, column_style=:nested);
