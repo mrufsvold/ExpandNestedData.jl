b2acae4a873dd22680f1378554c34943a0b7a055
Micah Rufsvold <mjrufsvold@mathematica-mpr.com>
2023-07-08 17:09:54 -0400

    update benchmarks

--- benchmarks/benchmarks.jl
+++ benchmarks/benchmarks.jl
@@ -9,7 +9,7 @@ small_dict = Dict(
 
 many_records = [
     small_dict
-    for _ in 1:1000
+    for _ in 1:10000
 ]
 
 function make_deep_dict(depth=1)
@@ -21,12 +21,18 @@ function make_deep_dict(depth=1)
         for i in 1:3
     )
 end
+deep_dict = make_deep_dict(7)
                 
+# expand(small_dict; lazy_columns=true, column_style=:nested)
 # @btime expand($small_dict; lazy_columns=true, column_style=:nested)
 # @profview ExpandNestedData.expand(small_dict; lazy_columns=true, column_style=:nested);
-# deep_dict = make_deep_dict(10)
+
+# expand(deep_dict; lazy_columns=true, column_style=:nested);
 # @btime expand($deep_dict; lazy_columns=true, column_style=:nested);
+# @profview_allocs ExpandNestedData.expand(deep_dict; lazy_columns=true, column_style=:nested) sample_rate=0.01
+
+# expand(many_records; lazy_columns=true, column_style=:nested);
+# @btime expand($many_records; lazy_columns=true, column_style=:nested);
 # @descend expand(many_records; lazy_columns=true, column_style=:nested)
 # @profview ExpandNestedData.expand(many_records; lazy_columns=true, column_style=:nested)
 # @profview_allocs ExpandNestedData.expand(many_records; lazy_columns=true, column_style=:nested)
-# @btime expand($many_records; lazy_columns=true, column_style=:nested);
