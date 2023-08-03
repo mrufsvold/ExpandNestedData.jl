ec5a873c507c23a16b6e28da562ba7a29d8ea000
Micah Rufsvold <mrufsvold@mathematica-mpr.com>
2023-08-02 23:17:16 -0400

    fix interpolation for make_switch

--- src/Utils.jl
+++ src/Utils.jl
@@ -98,9 +98,10 @@ function compose_switch_body(fs,lengths)
     end
     
     name = gensym("unvcat_switch")
+    error_str = "Attempted to access $total_len-length vector at index "
     func_def = :(
         function $(name)(i)
-            i > $total_len && error("Attempted to access $total_len-length vector at $i")
+            i > $(total_len) && error($error_str * "$i")
             $if_stmt
         end
     )
